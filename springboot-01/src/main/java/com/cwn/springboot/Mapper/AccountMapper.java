87a097a8f38edecf129b7aa5b886490e98ba2f31
cwn132 <19887460@qq.com>
2019-03-11 14:53:44 +0800

    1

---
+++ springboot-01/src/main/java/com/cwn/springboot/Mapper/AccountMapper.java
@@ -0,0 +1,33 @@
+package com.cwn.springboot.Mapper;
+
+import com.cwn.springboot.bean.Documentfile;
+import com.cwn.springboot.bean.Img;
+import com.cwn.springboot.bean.UserVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface AccountMapper {
+
+    void updateUserInfo(UserVO userVO);//更新用户信息
+
+    void updateHeadImgEmpty(Img img);//更新头像为空
+
+    void updateHeadImg(Img img);//更新头像图片
+
+    List<Map<String, Object>>  getDocumentfileByUser(Integer password);//根据用户名ID获取文件夹
+
+
+    public boolean insertDocumentfileByUser(Documentfile documentfile); //新建文件夹
+
+    public boolean deleteDocumentfileById(int documentfileid); //删除文件夹
+
+    public boolean deleteImgByDfId(int documentfileid); //删除图片
+
+    public boolean updateDocumentfileById(int documentfileid,String documentfilename);//更新文件夹
+
+    List<Map<String, Object>>  getImgByDocumentfile(Integer documentfileid);//根据文件夹ID获取图片
+
+}
---
+++ springboot-01/src/main/java/com/cwn/springboot/controller/LoginController.java
@@ -0,0 +1,80 @@
+package com.cwn.springboot.controller;
+
+import com.cwn.springboot.bean.UserVO;
+import com.cwn.springboot.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+
+    @Autowired
+    private IndexService indexService;
+
+    /**
+     * 登陆页面
+     * @param
+     * @return
+     */
+    @PostMapping(value="/login")
+    //@RequestMapping(value="/user/login",method = RequestMethod.POST)
+    public String lgoin(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Map<String,Object> map,
+                        HttpSession session){
+
+        UserVO userVO= indexService.getUserByUP(username,password);
+        String loginUser= (String) session.getAttribute("loginUser");
+        //设置session
+         session.setAttribute("loginUser",username);
+        if(userVO!=null){
+            if(username.equals("admin")){//管理员登陆
+
+                return "redirect:adminback/adminindex?pp="+loginUser;
+            }else{
+
+            //登陆成功
+            String headimg =indexService.getHeadImgpath(username);
+
+
+
+            map.put("user",userVO.getUsername());
+            map.put("usersigninfo",userVO.getUsersigninfo());
+            map.put("headimg",headimg);
+            map.put("loginUser",loginUser);
+
+            return "redirect:accountmain?pp="+loginUser+"&l=zh_CH";
+            }
+        } else{
+           //登陆失败
+            map.put("msg","用户名或用户密码错误！");
+            return "login";
+        }
+
+
+    }
+
+
+
+    /**
+     * 登陆页面
+     * @param
+     * @return
+     */
+    @PostMapping(value="/loginin")
+    //@RequestMapping(value="/user/login",method = RequestMethod.POST)
+    public HttpSession loginin(@RequestParam("username") String username,
+                               @RequestParam("password") String password){
+
+        UserVO userVO= indexService.getUserByUP(username,password);
+        HttpSession   session=null;
+        session.setAttribute("loginUser",userVO.getUsername());
+
+     return session;
+    }
+}
